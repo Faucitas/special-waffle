1d978e7b228073e6d99b75e512fd8baaf8ef41ef
Tommy Smith <tommy@tommyesmith.com>
2022-06-12 18:00:31 -0400

    initial commit

---
+++ assets/js/random.js
@@ -0,0 +1,21 @@
+let num_days = 3
+let dailyBudget = [5, 10, 15]
+let stores = ["Gas Station", "Drug Store", "Grocery Store", "Walmart", "Dollar Store"];
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+})
+
+let getRandomFromArray = function(values){
+	let randomIndex = Math.floor(Math.random() * values.length);
+	return values[randomIndex]
+}
+
+let randomBudget = getRandomFromArray(dailyBudget) * num_days;
+let randomStore = getRandomFromArray(stores);
+let formatBudget = formatter.format(randomBudget);
+document.getElementById("store").innerHTML = randomStore;
+document.getElementById("budget").innerHTML = formatBudget;
+document.getElementById("days").innerHTML = num_days;
